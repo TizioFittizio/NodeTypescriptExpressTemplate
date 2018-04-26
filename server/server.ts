68080f5427b9a6d51f8bc680c4fc221d164e0b51
C_Minor <luca.campa.94@gmail.com>
2018-04-26 20:51:37 +0200

    Setup config environment

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,15 @@
+require('./config/config');
 
 import express = require('express');
 import path = require('path');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('...');
+app.listen(3000, (hostname: string) => {
+    console.log(`Server started at port ${port}`);
 });
