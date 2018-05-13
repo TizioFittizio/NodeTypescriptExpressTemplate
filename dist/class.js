4125a598c5ffdd3febf532852e3fc4cc0eba7caf
C_Minor <luca.campa.94@gmail.com>
2018-05-13 20:01:34 +0200

    Removed class

--- dist/class.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Dio {
-    constructor() {
-    }
-    ;
-}
-exports.default = Dio;
-//# sourceMappingURL=class.js.map
